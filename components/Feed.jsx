a670aebd16f383979247f34c2525561474e1e3d6
Ankit Das <www.das5471@gmail.com>
2024-01-07 05:56:21 +0530

    data fetching for feed and some frontend done.

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,8 +1,58 @@
-import React from 'react'
+"use client";
 
-const Feed = () => {
+import {useState, useEffect} from 'react'
+
+import PromtCard from './PromtCard';
+
+
+const PromtCardList = ({data, handelTagClick}) => {
   return (
-    <div>Feed</div>
+    <div className='promt_layout mt-16'>
+      {data.map((promt, index) => (
+        <PromtCard key={promt._id} promt={promt} handelTagClick={handelTagClick}/>
+      ))}
+    </div>
+  )
+
+}
+ 
+const Feed = () => {
+  const [searchText, setSearchText] = useState('');
+  const [posts, setPosts] = useState([]);
+
+  const handelSearchChange = (e) => {
+    e.preventDefault();
+
+
+  }
+
+  useEffect(() => {
+    const fetchPromts = async () => {
+      const res = await fetch('/api/prompt');
+      const data = await res.json();
+      
+      setPosts(data);
+    }
+    fetchPromts();
+  },[])
+  // console.log(posts);
+  return (
+    <section className='feed'>
+      <form className='relative w-full flex-center'>
+        <input 
+          type="text" 
+          placeholder='Search for tag or username'
+          value={searchText}
+          onChange={handelSearchChange}
+          required
+          className='search_input peer'
+        />
+      </form>
+      <PromtCardList
+        data={posts}
+        handelTagClick={() => {}}
+      />
+    </section>
   )
 }
 
--- components/PromtCard.jsx
+++ components/PromtCard.jsx
@@ -1,8 +1,27 @@
-import React from 'react'
+"use client"
 
-const PromtCard = () => {
+import { useState } from 'react'
+import Image from 'next/image'
+import { useSession } from 'next-auth/react'
+import { usePathname, useRouter } from 'next/navigation'
+
+
+const PromtCard = ({ promt, handleTagClick, handleEdit, handleDelete }) => {
+  console.log(promt);
   return (
-    <div>PromtCard</div>
+    <div className='prompt_card'>
+      <div className='flex justify-between items-start gap-5'>
+        <div>
+          <Image 
+            src={promt.creator.image}
+            alt='profile image'
+            width={40}
+            height={40}
+            className='rounded-full object-contain'
+          />
+        </div>
+      </div>
+    </div>
   )
 }
 
